2bdd5f70128fdb111071288029210d837967dd06
Arnav Upadhyay <135106027+ArnavUpadhyay7@users.noreply.github.com>
2024-03-26 13:51:11 +0530

    Updated error.js
    Made it responsive for mobile devices

--- app/error.js
+++ app/error.js
@@ -4,12 +4,17 @@ import React from 'react'
 
 const error = () => {
   return (
-    <div className='h-screen bg-black text-white pt-40 text-center'>
+    <div className='h-screen bg-black text-white pt-40 text-center px-6'>
+    
       <h1 className='text-5xl'>Sorry,</h1>
       <h2 className='text-4xl pt-6'>The Item you searched for does not exist in our menu.</h2>
-      <div className='flex justify-center gap-10 pt-10'>
-      <Link className='md:px-16 px-8 md:py-5 py-4 text-[2vh] font-semibold bg-red-600 hover:bg-red-800 rounded-xl' href='https://forkify-api.herokuapp.com/phrases.html'> Check Our Menu. </Link>
-        <Link className='md:px-16 px-8 md:py-5 py-4 text-[2vh] font-semibold bg-red-600 hover:bg-red-800 rounded-xl' href='/'>Back to Home Page</Link>
+      <div className='md:flex justify-center gap-10 pt-12'>
+        <div>
+          <Link className='md:px-16 px-8 md:py-5 py-4 text-[2vh] font-semibold bg-red-600 hover:bg-red-800 rounded-xl' href='https://forkify-api.herokuapp.com/phrases.html'> Check Our Menu. </Link>
+        </div>
+        <div className='md:pt-0 pt-12'>
+          <Link className='md:px-16 px-8 md:py-5 py-4 text-[2vh] font-semibold bg-red-600 hover:bg-red-800 rounded-xl' href='/'>Back to Home Page</Link>  
+        </div>
       </div>
       
     </div>
