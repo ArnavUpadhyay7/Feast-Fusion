6b5d98ed6a013a0e6f9e8788e313da06551c59b8
ArnavUpadhyay7 <arnavupadhyay7@gmail.com>
2024-04-17 23:33:20 +0530

    UPDATED
    shows the number of items added to favourites

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,15 +1,18 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+
+  const favouriteItems = useSelector((store)=>store.favourites.items)
 
   return (
 
     // TO DO's
     // Make navbar fixed (sticky top-0) and bg-blur
     // make the toggleMode icon/button on navbar
-    // and use it's value in body (context or another slice in redux)
+    // and use it's value in body (another slice in redux)
     // Lazy load items
 
     <div className='pt-4'>
@@ -17,7 +20,7 @@ const Navbar = () => {
         <Link href="/pages/landing"><img className='size-12' src='https://cdn-icons-png.flaticon.com/128/562/562678.png'/></Link>
         <div className='flex md:gap-10 gap-4'>
           <Link href='/pages/items' className='text-2xl font-extralight hover:font-semibold'>Items</Link>
-          <Link href='/pages/favourites' className='text-2xl font-extralight hover:font-semibold'>Favourites</Link>
+          <Link href='/pages/favourites' className='md:text-2xl font-extralight hover:font-semibold'>Favourites - {favouriteItems.length} items</Link>
         </div>
       </div>
     </div>
